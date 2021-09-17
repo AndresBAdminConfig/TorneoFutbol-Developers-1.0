c4fc3c00e7c3a24394785a28eb5a759a5fc699e1
AndresBAdminConfig <andresefe18@gmail.com>
2021-09-17 14:40:43 -0500

    Creación Entidades equipos y jugadores
    se creo una carpeta en el capa dominio llamada Entidades y se crearon dos files uno de la entidad equipos y el otro de la entidad jugadores

---
+++ TorneoFutbol.App.Dominio/Entidades/equipos.cs
@@ -0,0 +1,10 @@
+namespace TorneoFutbol.App.Dominio
+{
+    public class equipos
+    {
+        // Identificador único de cada persona
+        public int  Id  { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
---
+++ TorneoFutbol.App.Dominio/Entidades/jugadores.cs
@@ -0,0 +1,12 @@
+namespace TorneoFutbol.App.Dominio
+{
+    public class jugadores
+    {
+        // Identificador único de cada persona
+        public int  Id  { get; set; }
+        public string Nombre { get; set; }
+        public string Numero { get; set; }
+        public string Posicion { get; set; }
+
+    }
+}
