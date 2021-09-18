991e5dc44f150d59a20d9f381e209c387b5bd05f
AndresBAdminConfig <andresefe18@gmail.com>
2021-09-17 22:05:13 -0500

    Creacion entidad Desempeño Equipos
    Se creo la entidad de desempeño equipos y se modifico la id de la entidad equipos

--- TorneoFutbol.App.Dominio/Entidades/equipos.cs
+++ TorneoFutbol.App.Dominio/Entidades/equipos.cs
@@ -3,7 +3,7 @@ namespace TorneoFutbol.App.Dominio
     public class Equipos
     {
         // Identificador único de cada persona
-        public int  Id  { get; set; }
+        public int  Id_Equipos  { get; set; }
         public string Nombre { get; set; }
 
     }
