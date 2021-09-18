984434e7598a8b52258b0afbabd575d1f27c6db9
KatheLondo <90867821+KatheLondo@users.noreply.github.com>
2021-09-17 21:53:15 -0500

    Adicion código DirectorTecnico y Estadio

---
+++ TorneoFutbol.App.Dominio/Entidades/DirectorTecnico.cs
@@ -0,0 +1,13 @@
+namespace TorneoFutbol.App.Dominio
+{
+    public class Arbitro
+    {
+        // Identificador único de cada persona
+        public int  Id  { get; set; }
+        public string Nombre { get; set; }
+        public string Documento { get; set; }
+        public string Telefono { get; set; }
+        
+
+    }
+}
---
+++ TorneoFutbol.App.Dominio/Entidades/Estadio.cs
@@ -0,0 +1,12 @@
+namespace TorneoFutbol.App.Dominio
+{
+    public class Arbitro
+    {
+        // Identificador único de cada persona
+        public int  Id  { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        
+
+    }
+}
