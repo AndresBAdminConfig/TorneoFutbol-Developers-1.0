55204c281f21209c81078bf9b817f1ac0eefa908
KatheLondo <90867821+KatheLondo@users.noreply.github.com>
2021-09-18 22:04:38 -0500

    instalacion Framework

--- TorneoFutbol.App.Persistencia/Class1.cs
+++ TorneoFutbol.App.Persistencia/Class1.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using TorneoFutbol.App.Dominio;
 
 namespace TorneoFutbol.App.Persistencia
 {
-    public class Class1
+    public class AppContext : DbContext
     {
+        public Dbset<Jugadores> Jugadores{get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data")
+            }
+        }
     }
 }
