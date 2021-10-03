ab30da07b5654585f12362571e7041ceb8ff519d
Maurem Chaparro <maurem.ch@gmail.com>
2021-10-03 11:15:09 -0500

    Index y startup Arbitros

--- TorneoFutbol.App.Frontend/Pages/Arbitros/Index.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Arbitros/Index.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Persistencia;
+using TorneoFutbol.App.Dominio;
 
 namespace TorneoFutbol.App.Frontend.Pages.Arbitros
 {
     public class IndexModel : PageModel
     {
+        private readonly IRepositorioArbitros _repoArbitros;
+        public IEnumerable<Arbitro> arbitros {get;set;}
+        public IndexModel(IRepositorioArbitros repoArbitros)
+        {
+            _repoArbitros = repoArbitros;
+        }
+
         public void OnGet()
         {
+            arbitros = _repoArbitros.GetAllArbitros();
         }
     }
 }
--- TorneoFutbol.App.Frontend/Startup.cs
+++ TorneoFutbol.App.Frontend/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TorneoFutbol.App.Persistencia;
+
 
 namespace TorneoFutbol.App.Frontend
 {
@@ -24,6 +26,7 @@ namespace TorneoFutbol.App.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IRepositorioArbitros, RespositorioArbitros>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
