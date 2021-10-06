ab81356602e44ce7724faadbe77221f192ba685f
Maurem Chaparro <maurem.ch@gmail.com>
2021-10-05 23:44:42 -0500

    Index Goles Arbitros

--- TorneoFutbol.App.Frontend/Pages/Arbitros/Index.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Arbitros/Index.cshtml.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.Pages.Arbitros
 {
     public class IndexModel : PageModel
     {
+        private readonly IRepositorioArbitros _repoArbitros;
+        public IEnumerable<Arbitro> arbitros {get; set;}
         public void OnGet()
         {
         }
--- TorneoFutbol.App.Frontend/Pages/Goles/Index.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/Goles/Index.cshtml.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.Pages.Goles
 {
     public class IndexModel : PageModel
     {
+        private readonly IRepositorioGoles _repoGoles;
+        public IEnumerable<Goles> goles {get; set;}
         public void OnGet()
         {
         }
