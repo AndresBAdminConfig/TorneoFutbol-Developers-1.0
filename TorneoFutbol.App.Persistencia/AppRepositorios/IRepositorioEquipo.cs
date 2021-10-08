c0cc6261267344287ebd9da44339455363c55771
Beltran-Andres <andresefe18@gmail.com>
2021-10-07 23:19:40 -0500

    Erreglo Repositorio Equipo

--- TorneoFutbol.App.Persistencia/AppRepositorios/IRepositorioEquipo.cs
+++ TorneoFutbol.App.Persistencia/AppRepositorios/IRepositorioEquipo.cs
@@ -1,17 +1,15 @@
-//using System.Collections.Generic;
-//using TorneoFutbol.App.Dominio;
+using System.Collections.Generic;
+using TorneoFutbol.App.Dominio;
 
-//namespace TorneoFutbol.App.Persistencia
-//{
+namespace TorneoFutbol.App.Persistencia
+{
 
-  //  public interface IRepositorioEquipo
-   // {
-     //   Equipo AddEquipo(Equipo Equipos);
-       // IEnumerable<Equipo> GetAllEquipo();
-        //void DeleteEquipo(int idEquipo);
-        //Equipo GetEquipo(int idEquipo);
-        //Equipo UpdateEquipo(Equipo Equipos);
-
-    //}
-
-//}
+    public interface IRepositorioEquipo
+    {
+        Equipo AddEquipo(Equipo Equipos);
+        IEnumerable<Equipo> GetAllEquipo();
+        void DeleteEquipo(int idEquipo);
+        Equipo GetEquipo(int idEquipo);
+        Equipo UpdateEquipo(Equipo Equipos);
+    }
+}
--- TorneoFutbol.App.Persistencia/AppRepositorios/RepositorioEquipo.cs
+++ TorneoFutbol.App.Persistencia/AppRepositorios/RepositorioEquipo.cs
@@ -1,44 +1,44 @@
-//using System.Collections.Generic;
-//using TorneoFutbol.App.Dominio;
-//namespace TorneoFutbol.App.Persistencia
-//{
-  //  public class RepositorioEquipo : IRepositorioEquipo
-    //{
+using System.Collections.Generic;
+using TorneoFutbol.App.Dominio;
+namespace TorneoFutbol.App.Persistencia
+{
+    public class RepositorioEquipo : IRepositorioEquipo
+    {
 
-      //  private readonly AppContext _appContext = new AppContext();   
+        private readonly AppContext _appContext = new AppContext();   
         
         
-        //Equipo IRepositorioEquipo.AddEquipo(Equipo Equipos)
-        //{
-          //  var equipoAdicionado = _appContext.equipo.Add(Equipos);
-            //_appContext.SaveChanges();
-            //return equipoAdicionado.Entity;
-        //}
-        //IEnumerable<Equipo> IRepositorioEquipo.GetAllEquipo()
-        //{
-          //  return _appContext.Equipos;
-        //}
-        //public void DeleteEquipo(int idEquipo)
-        //{
-          //  var EquipoEncontrado = _appContext.Equipos.Find(idEquipo);
-            //if (EquipoEncontrado == null)
-              //  return;
-            //_appContext.Equipos.Remove(EquipoEncontrado);
-            //_appContext.SaveChanges();
-        //}
-        //public Equipo GetEquipo(int idEquipo)
-        //{
-          //  return _appContext.Equipos.Find(idEquipo);
-        //}  
-        //public Equipo UpdateEquipo (Equipo Equipos)
-        //{
-          //  var EquipoEncontrado = _appContext.Equipos.Find(Equipos.Id);
-            //if (EquipoEncontrado != null)
-            //{
-              //  EquipoEncontrado.Nombre = Equipos.Nombre;
-                //_appContext.SaveChanges();
-            //}
-            //return EquipoEncontrado;
-        //} 
-    //}
-//}
+        Equipo IRepositorioEquipo.AddEquipo(Equipo Equipos)
+        {
+            var equipoAdicionado = _appContext.Equipos.Add(Equipos);
+            _appContext.SaveChanges();
+            return equipoAdicionado.Entity;
+        }
+        IEnumerable<Equipo> IRepositorioEquipo.GetAllEquipo()
+        {
+            return _appContext.Equipos;
+        }
+        public void DeleteEquipo(int idEquipo)
+        {
+            var EquipoEncontrado = _appContext.Equipos.Find(idEquipo);
+            if (EquipoEncontrado == null)
+                return;
+            _appContext.Equipos.Remove(EquipoEncontrado);
+            _appContext.SaveChanges();
+        }
+        public Equipo GetEquipo(int idEquipo)
+        {
+            return _appContext.Equipos.Find(idEquipo);
+        }  
+        public Equipo UpdateEquipo (Equipo Equipos)
+        {
+            var EquipoEncontrado = _appContext.Equipos.Find(Equipos.Id);
+            if (EquipoEncontrado != null)
+            {
+                EquipoEncontrado.Nombre = Equipos.Nombre;
+                _appContext.SaveChanges();
+            }
+            return EquipoEncontrado;
+        } 
+    }
+}
