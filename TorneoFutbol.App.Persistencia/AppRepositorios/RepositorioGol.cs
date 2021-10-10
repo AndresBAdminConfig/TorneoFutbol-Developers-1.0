60f3b2e447e276daedb234baf0b13f1ee895f0f1
Maurem Chaparro <maurem.ch@gmail.com>
2021-10-10 10:57:05 -0500

    Corrección Repositorio Gol

--- TorneoFutbol.App.Persistencia/AppRepositorios/RepositorioGol.cs
+++ TorneoFutbol.App.Persistencia/AppRepositorios/RepositorioGol.cs
@@ -10,29 +10,29 @@ namespace TorneoFutbol.App.Persistencia
         
         Gol IRepositorioGol.AddGoles(Gol goles)
         {
-            var golesAdicionado = _appContext.Goles.Add(goles);
+            var golesAdicionado = _appContext.Gol.Add(goles);
             _appContext.SaveChanges();
             return golesAdicionado.Entity;
         }
         IEnumerable<Gol> IRepositorioGol.GetAllGoles()
         {
-            return _appContext.Goles;
+            return _appContext.Gol;
         }
         public void DeleteGoles(int idGoles)
         {
-            var GolesEncontrado = _appContext.Goles.Find(idGoles);
+            var GolesEncontrado = _appContext.Gol.Find(idGoles);
             if (GolesEncontrado == null)
                 return;
-            _appContext.Goles.Remove(GolesEncontrado);
+            _appContext.Gol.Remove(GolesEncontrado);
             _appContext.SaveChanges();
         }
         public Gol GetGoles(int idGoles)
         {
-            return _appContext.Goles.Find(idGoles);
+            return _appContext.Gol.Find(idGoles);
         } 
         public Gol UpdateGoles(Gol goles)
         {
-            var GolesEncontrado = _appContext.Goles.Find(goles.Id);
+            var GolesEncontrado = _appContext.Gol.Find(goles.Id);
             if (GolesEncontrado != null)
             {
                 GolesEncontrado.MinutoGol=goles.MinutoGol;
