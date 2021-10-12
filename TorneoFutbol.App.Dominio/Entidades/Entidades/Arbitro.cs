be7a9c14308ebf42a6ec38be68280d4217317141
Maurem Chaparro <maurem.ch@gmail.com>
2021-10-12 11:21:26 -0500

    DataAnotations Arbitros

--- TorneoFutbol.App.Dominio/Entidades/Entidades/Arbitro.cs
+++ TorneoFutbol.App.Dominio/Entidades/Entidades/Arbitro.cs
@@ -1,12 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TorneoFutbol.App.Dominio
 {
     public class Arbitro
     {
         // Identificador único de cada persona
         public int  Id  { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [StringLength(10, ErrorMessage = "Máximo 10 caracteres")]
         public string Documento { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [StringLength(10, ErrorMessage = "Máximo 10 caracteres")]
         public string Telefono { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [StringLength(80, ErrorMessage = "Máximo 80 caracteres")]
         public string Colegio { get; set; }
 
     }
--- TorneoFutbol.App.Dominio/Entidades/Entidades/Gol.cs
+++ TorneoFutbol.App.Dominio/Entidades/Entidades/Gol.cs
@@ -1,10 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace TorneoFutbol.App.Dominio
 {
     public class Gol
     {
         // Identificador único de cada persona
         public int  Id { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [StringLength(19, ErrorMessage = "Máximo 19 caracteres, AAAA-MM-DD 00:00:00")]
         public DateTime MinutoGol { get; set; }
         //Relacion Goles con Jugador
         public Jugador jugador {get; set;}
