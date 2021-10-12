f6588091d64e42603d1be0b833a1fbbbad0eb179
Ldelgado1820 <madele1820@gmail.com>
2021-10-12 17:01:58 -0500

    Correciones...>

--- TorneoFutbol.App.Dominio/Entidades/Entidades/DirectorTecnico.cs
+++ TorneoFutbol.App.Dominio/Entidades/Entidades/DirectorTecnico.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 namespace TorneoFutbol.App.Dominio
 {
     public class DirectorTecnico
     {
         // Identificador único de cada persona
         public int  Id  { get; set; }
+    
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(20, ErrorMessage = "Maximo 20 caracteres")]
         public string Nombre { get; set; }
+       
+        [Required(ErrorMessage = "El documento es obligatorio")]
+        [StringLength(15, ErrorMessage = "Maximo 15 caracteres")]
         public string Documento { get; set; }
-        public string Telefono { get; set; }
         
-
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        [StringLength(15, ErrorMessage = "Maximo 15 caracteres")]
+        public string Telefono { get; set; }        
     }
 }
