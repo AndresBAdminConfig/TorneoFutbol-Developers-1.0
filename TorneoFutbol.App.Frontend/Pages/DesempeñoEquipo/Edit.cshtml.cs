b33008c99074425d0a0d0c2bb63135dc2a39034d
Darok2 <90783586+Darok2@users.noreply.github.com>
2021-10-12 20:55:39 -0500

    merge

--- TorneoFutbol.App.Frontend/Pages/DesempeñoEquipo/Edit.cshtml.cs
+++ TorneoFutbol.App.Frontend/Pages/DesempeñoEquipo/Edit.cshtml.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.pages.DesempeñoEquipo
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IrepositorioDesempeñoEquipo _repoDesempeño;
+        public DesempeñoEquipo Desempeño {get; set;}
+        public EditModel(IrepositorioDesempeñoEquipo repoDesempeño)
         {
+            _repoDesempeño = repoDesempeño;
+        }
+        public IActionResult OnGet(int id)
+        {
+            Desempeño = _repoDesempeño.getDesempeño(id);
+            if (Desempeño == null)
+            {
+                return = NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+        public IActionResult OnPost(DesempeñoEquipo Desempeño)
+        {
+            _repoDesempeño.UpdateDesempeñoEquipo(Desempeño);
+            return RedirectToPage("Index");
         }
     }
 }
